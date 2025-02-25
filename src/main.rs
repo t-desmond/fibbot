3cc50b08bdcbdd33711c3e5fb650bc133c74c2fd
anonym-266 <desmondtardzenyuy@gmail.com>
2025-02-25 15:56:58 +0100

    update: updated ci.yml to handle inputs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::env::args;
 fn main() {
     let args: Vec<String> = args().skip(1).collect();
 
-    let arg_1 = &args[0];
-    println!("argument 1 is {}", arg_1);
+    let argument_1 = &args[0];
+    let argument_2 = &args[1];
+    println!("argument 1 is {}", argument_1);
+    println!("argument 1 is {}", argument_2);
 }
