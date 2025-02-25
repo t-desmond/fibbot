d2bd99a9a5c21a5856b6144868ecd2275b8f298b
anonym-266 <desmondtardzenyuy@gmail.com>
2025-02-25 17:22:05 +0100

    update: main.rs to handle data

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,14 @@ use std::env::args;
 fn main() {
     let args: Vec<String> = args().skip(1).collect();
 
-    let argument_1 = &args[0];
-    let argument_2 = &args[1];
-    println!("argument 1 is {}", argument_1);
-    println!("argument 1 is {}", argument_2);
+    if args.is_empty(){
+        println!("no arguments supplied");
+        return;
+    } else if args.len() == 1 {
+        println!("Fibbot requires two parameters");
+    } else if args.len() ==2  {
+        let argument_1 = &args[0];
+        let argument_2 = &args[1];
+        println!("fiboot enabled succesfully with max_threshold: {}", argument_2);
+    }
 }
