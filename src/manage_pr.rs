11237ea445ef8c72bc73e0276ea0e37ea17eefdb
anonym-266 <desmondtardzenyuy@gmail.com>
2025-03-01 00:14:02 +0100

    added step to get pr number

--- src/manage_pr.rs
+++ src/manage_pr.rs
@@ -24,7 +24,7 @@ impl PullRequest {
         let client = Client::new();
         let response = client
             .post(&url)
-            .header("Authorization", format!("Bearer {}", github_token))
+            .header("Authorization", format!("{}", github_token))
             .header("User-Agent", "FibBot")
             .header("Accept", "application/vnd.github.full+json")
             .json(&serde_json::json!({ "body": pr_content }))
