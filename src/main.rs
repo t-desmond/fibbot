5a19d87ecce947b5d401e26b272863d8d4c4b241
anonym-266 <desmondtardzenyuy@gmail.com>
2025-03-01 03:55:54 +0100

    changed sequence of execution

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,56 @@
 mod fib;
 mod get_numbers;
 mod manage_pr;
+use dotenv::dotenv;
 use fib::Fibbonacci;
 use get_numbers::GetNumbers;
-use num_bigint::BigInt;
 use manage_pr::PullRequest;
-use  dotenv::dotenv;
+use num_bigint::BigInt;
 use std::env::{self};
 #[tokio::main]
-async fn main() ->  Result<(), Box<dyn std::error::Error>>{
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _ = dotenv().is_ok();
     // get environment variables
-    let github_repository = env::var("GITHUB_REPOSITORY").unwrap_or_else(|_| "t-desmond/fibbot".to_string());
-    let github_repository_vec=  github_repository.split("/").collect::<Vec<&str>>();
+    let github_repository =
+        env::var("GITHUB_REPOSITORY").unwrap_or_else(|_| "t-desmond/fibbot".to_string());
+    let github_repository_vec = github_repository.split("/").collect::<Vec<&str>>();
     let owner = github_repository_vec[0];
     let repo = github_repository_vec[1];
     let pr_number = env::var("PR_NUMBER")
-            .unwrap_or_else(|_| "1".to_string())
-            .parse::<u64>()
-            .expect("Invalid PR_NUMBER");
-    
-    // get pr content
-    let pr = PullRequest::get_pr(&owner, &repo, pr_number).await?;
-    let path = &pr.items.first().unwrap().patch.clone().unwrap();
-
-    println!("{:#?}", path);
-    println!("{:?}", get_numbers::GetNumbers::extract_numbers(path));
+        .unwrap_or_else(|_| "1".to_string())
+        .parse::<u64>()
+        .expect("Invalid PR_NUMBER");
 
     let enable_fib = env::var("INPUT_ENABLE_FIB").unwrap_or_else(|_| "true".to_string());
-    let max_threshold = env::var("INPUT_MAX_THRESHOLD").unwrap_or_else(|_| "100".to_string());
-    println!("Enable Fibonacci Calculation: {}", enable_fib);
-    println!("Max Threshold: {}", max_threshold);
-
     if enable_fib.eq("true") {
         println!("fibbot enabled...");
+
+        let max_threshold = env::var("INPUT_MAX_THRESHOLD").unwrap_or_else(|_| "100".to_string());
+        println!("Max Threshold: {}", max_threshold);
+
+        // get pr content
+        let pr = PullRequest::get_pr(&owner, &repo, pr_number).await?;
+        let path = &pr.items.first().unwrap().patch.clone().unwrap();
+
+        println!("Found : {:?}", get_numbers::GetNumbers::extract_numbers(path));
+
         let extracted_numbers = GetNumbers::extract_numbers(&path);
+        
         let fib_of_extracted_numbers: Vec<BigInt> = extracted_numbers
             .into_iter()
             .filter(|x| x < &max_threshold.parse::<u32>().unwrap())
             .map(|x| Fibbonacci::fibbo(x.into()))
             .collect();
-        println!("fib of found numbers are: {:?}", fib_of_extracted_numbers);
+        println!("fib of found numbers less than {} are: {:?}", max_threshold, fib_of_extracted_numbers);
 
         let comment_body = format!("{:?}", fib_of_extracted_numbers);
-        manage_pr::PullRequest::post_comment_to_pr( github_repository.as_str(), comment_body.as_str(), pr_number).await?;
+    
+        manage_pr::PullRequest::post_comment_to_pr(
+            github_repository.as_str(),
+            comment_body.as_str(),
+            pr_number,
+        )
+        .await?;
     } else {
         println!("fibbot disabled...")
     }
